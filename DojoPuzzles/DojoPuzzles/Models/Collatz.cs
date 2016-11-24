6d47451f27b822df18690224b90622ea3ea926a4
Gustavo Fernandes <guga.728@gmail.com>
2016-11-24 20:52:24 -0200

    Collatz Commit

--- DojoPuzzles/DojoPuzzles/Models/Collatz.cs
+++ DojoPuzzles/DojoPuzzles/Models/Collatz.cs
@@ -10,7 +10,14 @@ namespace DojoPuzzles.Models
         public List<CollatzModels> listCollatz = new List<CollatzModels>();
 
         /// <summary>
-        /// Calcular Collatz
+        /// Analisando a conjectura de Collatz
+        /// Para definir uma seqüência a partir de um número inteiro o positivo, temos as seguintes regras:
+        /// n → n/2 (n é par)
+        /// n → 3n + 1 (n é ímpar)
+        /// Usando a regra acima e iniciando com o número 13, geramos a seguinte seqüência:
+        /// 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+        /// Podemos ver que esta seqüência(iniciando em 13 e terminando em 1) contém 10 termos.Embora ainda não tenha sido provado(este problema é conhecido como Problema de Collatz), sabemos que com qualquer número que você começar, a seqüência resultante chega no número 1 em algum momento.
+        /// Desenvolva um programa que descubra qual o número inicial entre 1 e 1 milhão que produz a maior seqüência.
         /// </summary>
         /// <param name="collatzModels"></param>
         public void calcular(CollatzModels collatzModels)
@@ -25,8 +32,8 @@ namespace DojoPuzzles.Models
             while (!can_escape)
             {
                 number = ((number % 2) == 0) // verificar se "number" é divisível por 2
-                    ? (number / 2) // se "number" for par
-                    : ((3 * number) + 1);  // se "number" for ímpar
+                    ? (number / 2) // "number" → "number"/2 ("number" é par)
+                    : ((3 * number) + 1);  // "number" → (3 * "number") + 1 ("number" é ímpar)
 
                 result += " -> " + number.ToString();
                 can_escape = (number == 1) ? true : false; // Sair do while se "number" for 1
